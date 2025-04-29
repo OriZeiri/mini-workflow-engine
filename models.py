935b882382550308301c1d37b222a77c3ed99f2d
orize <orize@rafael.co.il>
2025-04-29 22:13:45 +0300

    added dump and load for the data that being saved to db

--- models.py
+++ models.py
@@ -25,5 +25,19 @@ class StepRuntime(StepInput):
     tasks: Dict[str, TaskStatus]
     step_idx: Optional[int] = None
 
+    def serialize(self) -> dict:
+        return {
+            "step_idx": self.step_idx,
+            "type": self.type.value,
+            "tasks": {task: status.value for task, status in self.tasks.items()}
+        }
+    @classmethod
+    def deserialize(cls, data: dict) -> "StepRuntime":
+        return cls(
+            step_idx=data["step_idx"],
+            type=StepType(data["type"]),
+            tasks={task: TaskStatus(status) for task, status in data["tasks"].items()}
+        )
+
 class WorkflowRequest(BaseModel):
     steps: List[StepInput] = STEPS_DEBUG_VALUE
